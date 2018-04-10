4096bcdab888b7dc2bbc388702122fff6c4845a4
Ryan Barker <ryangeorgebarker0@gmail.com>
2018-04-10 16:31:24 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,7 +1,7 @@
 def display_board
-puts top_row = [" ", " ", " ", "|", " ", " ", " ", "|", " ", " ", " "]
+puts top_row = ["   " + "|" + "   " + "|" + "   "]
 puts top_line = ["-----------"]
-puts middle_row = [" ", " ", " ", "|", " ", " ", " ", "|", " ", " ", " "]
+puts middle_row = ["   " + "|" + "   " + "|" + "   "]
 puts bottom_line = ["-----------"]
-puts bottom_row = [" ", " ", " ", "|", " ", " ", " ", "|", " ", " ", " "]
+puts bottom_row = ["   " + "|" + "   " + "|" + "   "]
 end
